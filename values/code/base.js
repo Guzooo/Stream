15d504ab12f27518990a4e5584dbf650c30f60dc
Guzooo <guz-m@o2.pl>
2024-01-06 13:28:09 +0100

    + metoda centrująca content strony

--- values/code/base.js
+++ values/code/base.js
@@ -2,11 +2,24 @@ const Base = (function(){
 
     //metody, które będą dostępne publicznie
     return {
-        initial
+        initial,
+        centerContent
     };
 
     function initial(){
         setContentPadding();
+        initialComponents();
+    }
+
+    function centerContent(){
+        const header = document.getElementsByTagName("header")[0];
+        const content = document.getElementsByTagName("main")[0];
+        const windowHeight = innerHeight;
+        if(header.offsetHeight + content.offsetHeight < windowHeight){
+            const centeredPadding = (windowHeight - header.offsetHeight - content.offsetHeight)/2
+            content.style.paddingTop = header.offsetHeight + centeredPadding;
+        }
+
     }
 
     function setContentPadding(){
@@ -15,6 +28,11 @@ const Base = (function(){
         content.style.paddingTop = header.offsetHeight;
     }
 
+    function initialComponents(){
+        window.mdc.autoInit();
+    }
+
 })();
+
 
 Base.initial();
