2e749658e6cf72622784c5f1189466294d99ca18
Guzooo <guz-m@o2.pl>
2024-01-06 03:55:17 +0100

    + reakcja na najechanie elementów na headerze

--- values/prefabs/headers.js
+++ values/prefabs/headers.js
@@ -10,13 +10,13 @@ const PrefabHeader = (function(){
             `<header class="mdc-top-app-bar">
                     <div class="mdc-top-app-bar__row">
                         <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-start">
-                            <a href="" class="mdc-button">
+                            <a href="" class="mdc-button mdc-top-app-bar__action-item">
                                 <span class="mdc-button__ripple"></span>
                                 <span class="mdc-button__label">Pomoc</span>
                             </a>
                         </section>
                         <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-end">
-                            <a href="" class="mdc-button">
+                            <a href="" class="mdc-button mdc-top-app-bar__action-item">
                                 <span class="mdc-button__ripple"></span>
                                 <span class="mdc-button__label">Wyloguj się</span>
                             </a>
@@ -31,28 +31,29 @@ const PrefabHeader = (function(){
                         <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-start">
                             <img src="../../values/images/logo64.png"/>
 
-                            <a href="" class="mdc-button">
+                            <a href="" class="mdc-button mdc-top-app-bar__action-item">
                                 <span class="mdc-button__ripple"></span>
                                 <span class="mdc-button__label">Nowości</span>
                             </a>
 
-                            <a href="" class="mdc-button">
+                            <a href="" class="mdc-button mdc-top-app-bar__action-item">
                                 <span class="mdc-button__ripple"></span>
                                 <span class="mdc-button__label">Twoje Gry</span>
                             </a>
 
-                            <a href="" class="mdc-button">
+                            <a href="" class="mdc-button mdc-top-app-bar__action-item">
                                 <span class="mdc-button__ripple"></span>
                                 <span class="mdc-button__label">Społeczność</span>
                             </a>
 
-                            <a href="" class="mdc-button">
+                            <a href="" class="mdc-button mdc-top-app-bar__action-item">
                                 <span class="mdc-button__ripple"></span>
                                 <span class="mdc-button__label">Profil</span>
                             </a>
                         </section>
                         <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-end">
-                            <button class="mdc-icon-button material-icons">
+                            <button class="mdc-icon-button material-icons mdc-top-app-bar__action-item">
+                                <div class="ripple-above-button-image mdc-icon-button__ripple"></div>
                                 search
                             </button>
                         </section>
