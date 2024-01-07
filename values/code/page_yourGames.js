e4d162509e5a660a143ef0ec126a108e0903c009
Jan Kwinta <94843940+KwintaJ@users.noreply.github.com>
2024-01-07 13:17:15 +0100

    Add files via upload

---
+++ values/code/page_yourGames.js
@@ -0,0 +1,47 @@
+//TODO
+//Tutaj powinna być logika do wybierania danych na podstawie userId + eksprot tabeli do osobnego pliku z tabelą
+const gamesData = [
+    {
+        "userId": 0,
+        "gameName": "Minecraft",
+        "downloaded": false,
+        "favorite": false
+    },
+    {
+        "userId": 0,
+        "gameName": "Fortnite",
+        "downloaded": true,
+        "favorite": true
+    },
+    {
+        "userId": 1,
+        "gameName": "CS",
+        "downloaded": true,
+        "favorite": false
+    },
+    {
+        "userId": 0,
+        "gameName": "MARIOPROS",
+        "downloaded": true,
+        "favorite": false
+    },
+    {
+        "userId": 0,
+        "gameName": "BABAJAGA",
+        "downloaded": true,
+        "favorite": false
+    },
+    {
+        "userId": 0,
+        "gameName": "TOMBFIGHTER",
+        "downloaded": false,
+        "favorite": false
+    },
+    {
+        "userId": 0,
+        "gameName": "LIGALEGO",
+        "downloaded": true,
+        "favorite": true
+    }
+];
+
--- values/prefabs/headers.js
+++ values/prefabs/headers.js
@@ -61,3 +61,57 @@ const PrefabHeader = (function(){
                 </header>`);
     }
 })();
+
+const PrefabVerificatorHeader = (function(){
+
+    //metody, które będą dostępne publicznie
+    return {
+        get
+    };
+
+    function get(){
+        document.write(
+            `<header class="mdc-top-app-bar">
+                    <div class="mdc-top-app-bar__row">
+                        <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-start">
+                            <a href="" class="mdc-button mdc-top-app-bar__action-item">
+                                <span class="mdc-button__ripple"></span>
+                                <span class="mdc-button__label">Pomoc</span>
+                            </a>
+                        </section>
+                        <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-end">
+                            <a href="" class="mdc-button mdc-top-app-bar__action-item">
+                                <span class="mdc-button__ripple"></span>
+                                <span class="mdc-button__label">Wyloguj się</span>
+                            </a>
+                        </section>
+                    </div>
+                    <div class="mdc-top-app-bar__row">
+                        <section class="mdc-top-app-bar__section">
+                            <hr>
+                        </section>
+                    </div>
+                    <div class="mdc-top-app-bar__row">
+                        <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-start">
+                            <img src="../../values/images/logo64.png"/>
+
+                            <a href="" class="mdc-button mdc-top-app-bar__action-item">
+                                <span class="mdc-button__ripple"></span>
+                                <span class="mdc-button__label">Projekty</span>
+                            </a>
+
+                            <a href="" class="mdc-button mdc-top-app-bar__action-item">
+                                <span class="mdc-button__ripple"></span>
+                                <span class="mdc-button__label">Profil</span>
+                            </a>
+                        </section>
+                        <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-end">
+                            <button class="mdc-icon-button material-icons mdc-top-app-bar__action-item">
+                                <div class="ripple-above-button-image mdc-icon-button__ripple"></div>
+                                search
+                            </button>
+                        </section>
+                    </div>
+                </header>`);
+    }
+})();
