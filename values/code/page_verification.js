fa4d8727ccb98f86bf8db721bf00f553e84d078a
Jan Kwinta <94843940+KwintaJ@users.noreply.github.com>
2024-01-21 19:38:48 +0100

    - niedzialajace funkcje

--- values/code/page_verification.js
+++ values/code/page_verification.js
@@ -1,18 +1,1 @@
-const PageVerifiaction = (function(){
 
-    //metody, które będą dostępne publicznie
-    return {
-        verify,
-        cancel
-    };
-
-    function verify(){
-        //TODO
-        //Zatwierdzenie gry
-    }
-
-    function cancel(){
-        //TODO
-        //Odrzucenie gry
-    }
-})();
