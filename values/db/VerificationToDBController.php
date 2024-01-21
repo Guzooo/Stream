fe2e1aa20ea67bf1c9b1327cbb30b0ab16545b22
Jan Kwinta <94843940+KwintaJ@users.noreply.github.com>
2024-01-21 21:01:30 +0100

    Dodanie obslugi weryfikacji

--- values/db/VerificationToDBController.php
+++ values/db/VerificationToDBController.php
@@ -17,10 +17,11 @@ $verifiedGameID = $_POST['ID'];
 $tworcaID = $_POST['tworcaID'];
 $gameName = $_POST['gameName']
 
-// Aktualizuj tabele w bazie danych
+// Aktualizuj tabele niezweryfikowane w bazie danych
 $sql1 = "DELETE FROM niezweryfikowane WHERE ID=$verifiedGameID";
 $result1 = $conn->query($sql1);
 
+// Aktualizuj tabele nowosci w bazie danych
 $sql2 = "INSERT INTO nowosci(ID, tworcaID, gameName) VALUES ($verifiedGameID, $tworcaID, $gameName)";
 $result2 = $conn->query($sql2);
 
