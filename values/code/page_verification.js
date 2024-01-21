48780c839f5eca3fa5764246303c26b981ebd107
Jan Kwinta <94843940+KwintaJ@users.noreply.github.com>
2024-01-21 17:37:28 +0100

    Create page_verification.js

---
+++ values/code/page_verification.js
@@ -0,0 +1,18 @@
+const PageVerifiaction = (function(){
+
+    //metody, które będą dostępne publicznie
+    return {
+        verify,
+        cancel
+    };
+
+    function verify(){
+        //TODO
+        //Zatwierdzenie gry
+    }
+
+    function cancel(){
+        //TODO
+        //Odrzucenie gry
+    }
+})();
